50a956dfea48c6e55b781956fdc424ff921a459b
manoj <manojpachipala15@gmail.com>
2022-01-27 20:53:49 +0530

    bulb

---
+++ app.js
@@ -0,0 +1,48 @@
+// comment
+alert("hii");
+
+
+var greeting = "manoj";
+console.log(greeting);
+
+let age=22;
+console.log(age);
+
+let pi=3.124;
+console.log(pi);
+
+//operators
+
+//let add=2+3;
+//console.log(add);
+
+//let num1 = 2;
+//let num2 = 4;
+//let sum = num1+num2;
+//console.log(sum);
+
+function sum(num1, num2)
+{
+    let  add = num1 + num2;
+    console.log(add);
+}
+sum (2,3);
+sum(10,5);
+
+
+function alertMessage(message){
+    alert(message);
+}
+
+alertMessage("manoj");
+alertMessage("swipe");
+
+let ag = 18;
+
+if(ag<18)
+{
+    alert(" you are an adult");
+}
+else{
+    alert("you are  not an adult");
+}
---
+++ app1.js
@@ -0,0 +1,11 @@
+ var bulb = document.getElementById("bulb")
+
+ function bulb_on()
+ {
+     bulb.src="https://cdn.pixabay.com/photo/2019/09/29/22/06/light-bulb-4514505_960_720.jpg"
+}
+
+function bulb_off()
+{
+    bulb.src=""
+}
